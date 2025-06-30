71523ddc3c0def795894a8bc9a2efc1d8930927d
LuisCas18 <Luis.cas18@outlook.com>
2025-06-30 13:18:27 -0600

    Implementación de Frontend tailwindCSS 4

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Temas del Curso</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 text-gray-900">
+    <div class="container mx-auto mt-10">
+        @yield('content')
+    </div>
+    @stack('scripts')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\CourseTopicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('course-topics', CourseTopicController::class);
